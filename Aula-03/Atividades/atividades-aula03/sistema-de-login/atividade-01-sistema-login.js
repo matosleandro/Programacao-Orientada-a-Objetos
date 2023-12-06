c6ffc61b29e1fb7f1ca6311041e8f087a409d512
Leandro Matos <leandro.matos@live.com>
2023-12-06 19:16:38 -0300

    fix: realizado algumas modificações no código

--- Aula-03/Atividades/atividades-aula03/sistema-de-login/atividade-01-sistema-login.js
+++ Aula-03/Atividades/atividades-aula03/sistema-de-login/atividade-01-sistema-login.js
@@ -1,43 +1,64 @@
 class SistemaDeLogin {
-    constructor() {
-      this.usuarios = [];
+  constructor() {
+    this.usuarios = [];
+  }
+
+  cadastrarUsuario(nome, senha) {
+    // Verifica se o usuário já existe usando indexOf
+    const usuarioExistenteIndex = this.usuarios.findIndex((usuario) => usuario.nome === nome);
+
+    if (usuarioExistenteIndex !== -1) {
+      console.log(`Usuário ${nome} já existe. Escolha outro nome de usuário.`);
+      return;
     }
-  
-    cadastrarUsuario(nome, senha) {
-      this.usuarios.push({ nome, senha });
-      console.log(`Usuário ${nome} cadastrado com sucesso.`);
+
+    // Verifica se a senha é válida (adicione suas próprias regras de validação)
+    if (senha.length < 6) {
+      console.log("A senha deve ter pelo menos 6 caracteres. Tente novamente.");
+      return;
     }
-  
-    realizarLogin(nome, senha) {
+
+    // Se passar nas verificações, cadastra o usuário
+    this.usuarios.push({ nome, senha });
+    console.log(`Usuário ${nome} cadastrado com sucesso.`);
+  }
+
+  realizarLogin(nome, senha) {
+    try {
       const usuarioEncontrado = this.usuarios.find(
         (usuario) => usuario.nome === nome && usuario.senha === senha
       );
-  
-      if (usuarioEncontrado) {
-        console.log(`Login bem-sucedido para ${nome}.`);
-        return nome;
-      } else {
-        console.log("Nome de usuário ou senha incorretos. Tente novamente.");
-        return null;
+
+      if (!usuarioEncontrado) {
+        throw new Error("Nome de usuário ou senha incorretos. Tente novamente.");
       }
-    }
-  
-    exibirMensagemPersonalizada(nome) {
-      return `Bem-vindo, ${nome}!`;
+
+      console.log(`Login bem-sucedido para ${nome}.`);
+      return nome;
+    } catch (error) {
+      console.error(error.message);
+      alert(error.message); // Exibe um alerta com a mensagem de erro
+      return null;
     }
   }
-  
-  // Exemplo de Uso:
-  
-  // Criando instância do sistema de login
-  const sistemaLogin = new SistemaDeLogin();
-  
-  // Cadastrando usuários
-  sistemaLogin.cadastrarUsuario("usuario1", "senha123");
-  sistemaLogin.cadastrarUsuario("usuario2", "abc456");
-  
-  // Realizando login e exibindo mensagem personalizada
-  const usuarioLogado = sistemaLogin.realizarLogin("usuario1", "senha123");
-  if (usuarioLogado) {
-    console.log(sistemaLogin.exibirMensagemPersonalizada(usuarioLogado));
+
+  exibirMensagemPersonalizada(nome) {
+    return `Bem-vindo, ${nome}!`;
   }
+}
+
+// Exemplo de Uso:
+
+// Criando instância do sistema de login
+const sistemaLogin = new SistemaDeLogin();
+
+// Cadastrando usuários
+sistemaLogin.cadastrarUsuario("usuario1", "senha123");
+sistemaLogin.cadastrarUsuario("usuario2", "abc456");
+sistemaLogin.cadastrarUsuario("usuario1", "outrasenha"); // Tentativa de cadastro repetido
+
+// Realizando login e exibindo mensagem personalizada
+const usuarioLogado = sistemaLogin.realizarLogin("usuario1", "senha123");
+if (usuarioLogado) {
+  console.log(sistemaLogin.exibirMensagemPersonalizada(usuarioLogado));
+}
